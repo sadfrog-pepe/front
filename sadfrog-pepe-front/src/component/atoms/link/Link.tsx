7ae7d84345fc3427e967828b6012c47b7bff0475
bkdragon0228 <rlaqjarb2067@naver.com>
2022-10-21 20:33:49 +0900

    feat : link & logo tsx로 변경

---
+++ sadfrog-pepe-front/src/component/atoms/link/Link.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Link.module.css';
+import classnames from 'classnames';
+import styled, { css } from 'styled-components';
+
+export enum LinkTheme {
+    DEFAULT = 'default',
+}
+
+export enum LinkSize {
+    SMALL = 'small',
+    MEDIUM = 'medium',
+    LARGE = 'large',
+}
+
+export interface LinkProps {
+    to: string;
+    children: string;
+    theme?: LinkTheme;
+    size?: LinkSize;
+    color?: string;
+    className?: string;
+}
+const LinkComponent = (props: LinkProps) => {
+    const { to, children, theme, size, color, className } = props;
+
+    const classProps = classnames(
+        styles.link,
+        styles[theme ? theme : ''],
+        styles[size ? size : ''],
+        styles[color ? color : ''],
+        className
+    );
+
+    return (
+        <Link to={to} className={classProps}>
+            {children}
+        </Link>
+    );
+};
+
+export default LinkComponent;
+
+// atomic을 어떤 방식으로 적용할지에 대한 고민
+// styled-component vs css
+// export const StyledLink = styled.a`
+//     font-size: 0.9em;
+//     font-weight: 600;
+//     text-transform: uppercase;
+//     border-radius: 3px;
+//     cursor: pointer;
+//     text-decoration: none;
+//     color: black;
+//     background-color: white;
+//     border: none;
+
+//     /* size */
+//     ${(props) =>
+//         props.large &&
+//         css`
+//             font-size: 1.2rem;
+//         `}
+
+//     ${(props) =>
+//         props.medium &&
+//         css`
+//             font-size: 0.9rem;
+//         `}
+
+//     ${(props) =>
+//         props.small &&
+//         css`
+//             font-size: 0.7rem;
+//             color: dodgerblue;
+//         `}
+// `;
--- sadfrog-pepe-front/src/component/atoms/logo/Logo.tsx
+++ sadfrog-pepe-front/src/component/atoms/logo/Logo.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 import { LOGO_URL } from '../../../constant';
 
+interface LogoProps {
+    width: string;
+    height: string;
+}
+
 export const Logo = styled.div`
-    width: ${(props) => props.width || '200px'};
-    height: ${(props) => props.height || '100px'};
+    width: ${(props: LogoProps) => props.width || '200px'};
+    height: ${(props: LogoProps) => props.height || '100px'};
     background-image: url(${LOGO_URL});
     background-repeat: no-repeat;
     background-size: contain;
