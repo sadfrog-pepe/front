2e9174c0c1159b7a16b9a7cf738483e4f750ce9f
bkdragon0228 <rlaqjarb2067@naver.com>
2022-11-04 14:41:33 +0900

    feat : redux 미들웨어 추가, redux에서 promise 처리

--- sadfrog-pepe-front/src/main.jsx
+++ sadfrog-pepe-front/src/main.jsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './GlobalStyle.jsx';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer);
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <GlobalStyle />
-        <Provider store={store}>
+        <Provider store={store(rootReducer, composeWithDevTools())}>
             <App />
         </Provider>
     </React.StrictMode>
